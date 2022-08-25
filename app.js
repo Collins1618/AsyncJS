6be3558a7613485d0ca956c4731d86ff5e2037d2
Collins Kariuki <collinsmunkary@gmail.com>
2022-08-25 08:33:14 +0300

    using callbacks and the generic function to change background color

--- app.js
+++ app.js
@@ -29,10 +29,24 @@
 // },1000);
 
 
-const delayedColorChange = (newColor, delay) => {
+const delayedColorChange = (newColor, delay, doNext) => {
     setTimeout(() => {
         document.body.style.backgroundColor = newColor;
+        doNext && doNext(); //check if doNext exists, otherwise it 
+        //will be undefined
     }, delay)
 }
 
-delayedColorChange('olive', 3000);
+delayedColorChange('red', 1000, () => {
+    delayedColorChange('orange', 1000, () =>{
+        delayedColorChange('yellow', 1000, () =>{
+            delayedColorChange('green', 1000, () =>{
+                delayedColorChange('blue', 1000, () =>{
+                    delayedColorChange('indigo', 1000, () =>{
+        
+                    })
+                })
+            })
+        })
+    })
+});
