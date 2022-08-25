c8d286c8de9d85e9d1b3347d817595beb465c314
Collins Kariuki <collinsmunkary@gmail.com>
2022-08-25 08:18:57 +0300

    use settimeout to change background color after ever 1sec, follows the colors of the rainbow

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-console.log('Sending request to server');
-setTimeout(() => {
-    console.log("Here is your data from the server...")
-}, 3000);
+// console.log('Sending request to server');
+// setTimeout(() => {
+//     console.log("Here is your data from the server...")
+// }, 3000);
 
-console.log("I AM AT THE END OF THE FILE!")
+// console.log("I AM AT THE END OF THE FILE!")
+
+
+setTimeout(() => {
+    document.body.style.backgroundColor = 'red';
+    setTimeout(() => {
+        document.body.style.backgroundColor = 'orange';
+        setTimeout(() => {
+            document.body.style.backgroundColor = 'yellow';
+            setTimeout(() => {
+                document.body.style.backgroundColor = 'green';
+                setTimeout(() => {
+                    document.body.style.backgroundColor = 'blue';
+                    setTimeout(() => {
+                        document.body.style.backgroundColor = 'indigo';
+                        setTimeout(() => {
+                            document.body.style.backgroundColor = 'violet';
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
