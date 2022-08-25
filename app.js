18170271b2f018ed097cb0b35a808543c7996d73
Collins Kariuki <collinsmunkary@gmail.com>
2022-08-25 08:22:57 +0300

    generic function for background color change

--- app.js
+++ app.js
@@ -6,24 +6,33 @@
 // console.log("I AM AT THE END OF THE FILE!")
 
 
-setTimeout(() => {
-    document.body.style.backgroundColor = 'red';
+// setTimeout(() => {
+//     document.body.style.backgroundColor = 'red';
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = 'orange';
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = 'yellow';
+//             setTimeout(() => {
+//                 document.body.style.backgroundColor = 'green';
+//                 setTimeout(() => {
+//                     document.body.style.backgroundColor = 'blue';
+//                     setTimeout(() => {
+//                         document.body.style.backgroundColor = 'indigo';
+//                         setTimeout(() => {
+//                             document.body.style.backgroundColor = 'violet';
+//                         },1000);
+//                     },1000);
+//                 },1000);
+//             },1000);
+//         },1000);
+//     },1000);
+// },1000);
+
+
+const delayedColorChange = (newColor, delay) => {
     setTimeout(() => {
-        document.body.style.backgroundColor = 'orange';
-        setTimeout(() => {
-            document.body.style.backgroundColor = 'yellow';
-            setTimeout(() => {
-                document.body.style.backgroundColor = 'green';
-                setTimeout(() => {
-                    document.body.style.backgroundColor = 'blue';
-                    setTimeout(() => {
-                        document.body.style.backgroundColor = 'indigo';
-                        setTimeout(() => {
-                            document.body.style.backgroundColor = 'violet';
-                        },1000);
-                    },1000);
-                },1000);
-            },1000);
-        },1000);
-    },1000);
-},1000);
+        document.body.style.backgroundColor = newColor;
+    }, delay)
+}
+
+delayedColorChange('olive', 3000);
